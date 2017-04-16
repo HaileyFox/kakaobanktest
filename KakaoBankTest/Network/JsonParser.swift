f8ab4df33703f93849ec5f12d4bd090f08d54f92
HaileyFox <angelrena24@gmail.com>
2017-04-17 01:43:38 +0900

    Update JsonParser.swift

--- KakaoBankTest/Network/JsonParser.swift
+++ KakaoBankTest/Network/JsonParser.swift
@@ -54,36 +54,6 @@ class JsonParser: NSObject {
         rankDetailModel.rating = (detailDic.object(forKey: "averageUserRating") as! NSNumber?)?.stringValue
         rankDetailModel.screenShots = detailDic.object(forKey: "screenshotUrls") as? NSArray
         rankDetailModel.appInfo = detailDic.object(forKey: "description") as? String
-//        
-//        let feed = jsonDic.object(forKey: "feed") as! NSDictionary
-//        
-//        guard let entry = feed.object(forKey: "entry") as? NSArray  else {
-//            return resultArray
-//            
-//        }
-//        
-//        print("entry : \(entry)")
-//        
-//        for entryData in entry {
-//            let dataDic = entryData as! NSDictionary
-//            let listModel = RankListDataModel()
-//            
-//            let id = dataDic.object(forKey: "id") as! NSDictionary
-//            let attributes = id.object(forKey: "attributes") as! NSDictionary
-//            listModel.itemId = attributes.object(forKey: "im:id") as? String
-//            
-//            let imImage = dataDic.object(forKey: "im:image") as! NSArray
-//            let imageItem = imImage[2] as! NSDictionary
-//            listModel.image = imageItem.object(forKey: "label") as? String
-//            
-//            let imName = dataDic.object(forKey: "im:name") as! NSDictionary
-//            listModel.title = imName.object(forKey: "label") as? String
-//            
-//            resultArray.add(listModel)
-//        }
-//        
-//        print("resultArray : \(resultArray)")
-        
         
         return rankDetailModel
     }
